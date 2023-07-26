18e36a705dcd8dc984b4b7e9c62bf1b6daf54755
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-26 15:32:52 -0400

    troubleshooting 400 error

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const title = document.querySelector('#blog-title').value.trim();
+    const title = document.querySelector('#blog-name').value.trim();
     // const needed_funding = document.querySelector('#project-funding').value.trim();
     const content = document.querySelector('#blog-content').value.trim();
   
     if (title && content) {
       const response = await fetch(`/api/blog`, {
         method: 'POST',
-        body: JSON.stringify({ title, content, date_created }),
+        body: JSON.stringify({ title, content }),
         headers: {
           'Content-Type': 'application/json',
         },
