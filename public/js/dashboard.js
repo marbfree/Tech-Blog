0c8446e4d1c74ca0a97ea3514b536bc915032304
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-27 12:12:31 -0400

    connect to jawsdb

---
+++ public/js/dashboard.js
@@ -0,0 +1,21 @@
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blog/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete blog');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blog-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document.querySelector('.blog-list')?.addEventListener('click', delButtonHandler);
--- public/js/profile.js
+++ public/js/profile.js
@@ -21,24 +21,24 @@ const newFormHandler = async (event) => {
     }
   };
   
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+  // const delButtonHandler = async (event) => {
+  //   if (event.target.hasAttribute('data-id')) {
+  //     const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/blog/${id}`, {
-        method: 'DELETE',
-      });
+  //     const response = await fetch(`/api/blog/${id}`, {
+  //       method: 'DELETE',
+  //     });
   
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete blog');
-      }
-    }
-  };
+  //     if (response.ok) {
+  //       document.location.replace('/profile');
+  //     } else {
+  //       alert('Failed to delete blog');
+  //     }
+  //   }
+  // };
   
-  document
-    .querySelector('.new-blog-form')
-    .addEventListener('submit', newFormHandler);
+  // document
+  //   .querySelector('.new-blog-form')
+  //   .addEventListener('submit', newFormHandler);
   
-  document.querySelector('.blog-list')?.addEventListener('click', delButtonHandler);
+  // document.querySelector('.blog-list')?.addEventListener('click', delButtonHandler);
